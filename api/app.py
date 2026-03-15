ef46993d2b3fb091d3714eafc4d65590395c17b6
mojtaba <m.tahmasbi0111@yahoo.com>
2026-03-15 22:25:52 +0330

    update

--- api/app.py
+++ api/app.py
@@ -1,15 +1,53 @@
+import uuid
+import requests
+from fastapi import FastAPI
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
 # static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # templates
 templates = Jinja2Templates(directory="templates")
+
+
+CENTRIFUGO_API = "http://centrifugo:8000/api"
+API_KEY = "apikey"
+
+
+@app.get("/create-room")
+def create_room():
+    room_id = str(uuid.uuid4())[:8]
+    return {
+        "room_id": room_id,
+        "url": f"/room/{room_id}"
+    }
+
+
+@app.post("/send")
+def send_message(room: str, user: str, text: str):
+
+    payload = {
+        "method": "publish",
+        "params": {
+            "channel": room,
+            "data": {
+                "user": user,
+                "text": text
+            }
+        }
+    }
+
+    requests.post(
+        CENTRIFUGO_API,
+        json=payload,
+        headers={"Authorization": f"apikey {API_KEY}"}
+    )
+
+    return {"status": "ok"}
 
 
 @app.get("/", response_class=HTMLResponse)
