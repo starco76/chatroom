2fcc020183d11d239ef47ab726a1966157e2ca0f
mojtaba <m.tahmasbi0111@yahoo.com>
2026-03-16 00:19:56 +0330

    update

--- static/chat.js
+++ static/chat.js
@@ -40,41 +40,41 @@ function startChat() {
   //     }, 2000);
   //   }
   // });
-    const centrifuge = new Centrifuge(
-      `ws://${window.location.hostname}:8008/connection/websocket`,
-      { token: window.token },
-    );
-    centrifuge.connect();
+  const centrifuge = new Centrifuge(
+    `ws://${window.location.hostname}:8008/connection/websocket`,
+    { token: window.token },
+  );
+  centrifuge.connect();
 
-    centrifuge.on("connect", (ctx) => {
-      log("✅ Connected: " + JSON.stringify(ctx));
-    });
+  centrifuge.on("connect", (ctx) => {
+    console.log("✅ Connected: " + JSON.stringify(ctx));
+  });
 
-    centrifuge.on("disconnect", (ctx) => {
-      log("❌ Disconnected: " + JSON.stringify(ctx));
-    });
+  centrifuge.on("disconnect", (ctx) => {
+    console.log("❌ Disconnected: " + JSON.stringify(ctx));
+  });
 
-    centrifuge.on("subscribing", function (ctx) {
-      console.log("subscribing");
-    });
+  centrifuge.on("subscribing", function (ctx) {
+    console.log("subscribing");
+  });
 
-    centrifuge.on("subscribed", function (ctx) {
-      console.log("subscribed");
-    });
+  centrifuge.on("subscribed", function (ctx) {
+    console.log("subscribed");
+  });
 
-    centrifuge.on("unsubscribed", function (ctx) {
-      console.log("unsubscribed");
-    });
-    centrifuge.on("publication", (ctx) => {
-      log("📨 Message: " + JSON.stringify(ctx.data));
-      // const msg = ctx.data;
-      // const div = document.createElement("div");
-      // div.className = "msg";
-      // div.innerHTML = `<b>${msg.user}</b>: ${msg.text}`;
-      // chatDiv.appendChild(div);
-      // chatDiv.scrollTop = chatDiv.scrollHeight;
-    });
-  };
+  centrifuge.on("unsubscribed", function (ctx) {
+    console.log("unsubscribed");
+  });
+  centrifuge.on("publication", (ctx) => {
+    console.log("📨 Message: " + JSON.stringify(ctx.data));
+    // const msg = ctx.data;
+    // const div = document.createElement("div");
+    // div.className = "msg";
+    // div.innerHTML = `<b>${msg.user}</b>: ${msg.text}`;
+    // chatDiv.appendChild(div);
+    // chatDiv.scrollTop = chatDiv.scrollHeight;
+  });
+}
 
 function send() {
   const text = document.getElementById("msg").value;
